958cd4841ee793925c11ffe3d4d37f888e48a8a3
mscreation <makwanashyam28@gmail.com>
2020-03-25 23:03:27 +0530

    1

--- covid19app/urls.py
+++ covid19app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from covid19app.views import index
+from covid19app.views import index,data
 from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', index),
+    url(r'data/$',data)
 ]
--- covid19app/views.py
+++ covid19app/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 def index(request):
     return render(request,'index.html')
+
+def data(request):
+    print(request.GET['contry'])
+    return render(request,'data.html')
